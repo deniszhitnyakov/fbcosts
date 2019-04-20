bb65938dd1c190a9bcfc053f6274949483bbd0fa
Denis Zhitnyakov <denis.zhitnyakov@gmail.com>
2019-04-20 15:59:37 +0300

    Update fbcosts_install.sh

--- fbcosts_install.sh
+++ fbcosts_install.sh
@@ -2,3 +2,9 @@
 apt update
 apt --assume-yes install apache2
 apt --assume-yes install php libapache2-mod-php php-curl
+apt --assume-yes install unzip
+apache2ctl restart
+cd /var/www/html
+wget https://github.com/deniszhitnyakov/fbcosts/archive/master.zip
+unzip master.zip
+mv -R fbcosts-master fbcosts
